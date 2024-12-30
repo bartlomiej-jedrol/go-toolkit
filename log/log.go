c393f192d33570a5ba09cf39397da61b1b80e5c9
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-12-30 12:46:08 +0100

    wip

--- log/log.go
+++ log/log.go
@@ -10,20 +10,26 @@ import (
 
 const (
 	Enpoint  string = "endpoint"
-	Service  string = "serviceName"
+	Service  string = "service"
 	Function string = "function"
-	EnvVar   string = "environmentVariable"
+	EnvVar   string = "environment_variable"
 )
 
 func New() (*zap.Logger, error) {
 	config := zap.NewProductionConfig()
 	config.DisableStacktrace = true
 	config.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
+	config.DisableCaller = false
+
 	logger, err := config.Build()
 	if err != nil {
 		log.Printf("ERROR: New - failed to initiliaze zap logger")
 		return nil, err
 	}
+
+	// 0 - log.go as a caller
+	// 1 - actual caller
+	logger = logger.WithOptions(zap.AddCallerSkip(1))
 	return logger, nil
 }
 
@@ -33,8 +39,23 @@ func Error(msg, service, function string, endpoint, envVar *string) {
 		return
 	}
 
-	logger.Error(msg,
-		zap.String(Service, service),
-		zap.String(Function, function),
-		zap.String("fileId", "id"))
+	if msg == "" {
+		msg = "no message"
+	}
+
+	fields := []zapcore.Field{}
+	if service != "" {
+		fields = append(fields, zap.String(Service, service))
+	}
+	if function != "" {
+		fields = append(fields, zap.String(Function, function))
+	}
+	if endpoint != nil {
+		fields = append(fields, zap.String(Enpoint, *endpoint))
+	}
+	if envVar != nil {
+		fields = append(fields, zap.String(EnvVar, *envVar))
+	}
+
+	logger.Error(msg, fields...)
 }
